d2b5bbf7295f10e0cb05c089e31ecdf8ab131ebd
Valera Demin <wahrwolffe@gmail.com>
2023-09-05 02:28:31 +0300

    fixture usage + mark.parametrize

--- tests/test_list_func.py
+++ tests/test_list_func.py
@@ -8,5 +8,6 @@ def some_fixt():
     return [1, 2, 3]
 
 
-def test_app_list(some_fixt):
-    assert app_list(some_fixt, "book") == [1, 2, 3, "book"]
+@pytest.mark.parametrize("item, expected_result", [(4, [1, 2, 3, 4])])
+def test_app_list(some_fixt, item, expected_result):
+    assert app_list(some_fixt, item) == expected_result
