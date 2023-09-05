1847fc624fdde4a137f524ee51bb90611ede04b4
Valera Demin <wahrwolffe@gmail.com>
2023-09-05 08:07:46 +0300

    app_list func to 100% test coverage

--- tests/test_list_func.py
+++ tests/test_list_func.py
@@ -10,6 +10,7 @@ def some_fixt():
 
 @pytest.mark.parametrize("item, expected_result", [(4, [1, 2, 3, 4]),
                                                    (5, [1, 2, 3, 5]),
-                                                   ("book", [1, 2, 3, "book"])])
+                                                   ("book", [1, 2, 3, "book"]),
+                                                   (None, [1, 2, 3])])
 def test_app_list(some_fixt, item, expected_result):
     assert app_list(some_fixt, item) == expected_result
